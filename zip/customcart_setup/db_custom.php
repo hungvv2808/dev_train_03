c8c15325426684c17e38e7560dc1ee09be00f12c
hungvv2808 <vuhung2881999@gmail.com>
2021-01-21 11:58:09 +0700

    2nd commit

---
+++ zip/customcart_setup/db_custom.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 21/02/2019
+ * Time: 10:09
+ */
+return array(
+    'cart_type' => 'custom',
+    'db_host' => '127.0.0.1',
+    'db_user' => 'root',
+    'db_password' => '1111',
+    'db_name' => 'customcart',
+    'db_prefix' => '',
+    'image_dir' => '',
+    'image_dir_product' => '',
+    'image_dir_category' => '',
+    'version' => '',
+);
